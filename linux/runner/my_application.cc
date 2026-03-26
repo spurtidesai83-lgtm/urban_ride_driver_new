b61e55781f45c814e636154edbdde05dc9e48f1a
Ansh Sharma <ansharma013@gmail.com>
2026-03-26 22:39:30 +0530

    ICon UPDate

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "urban_ride_driver_new");
+    gtk_header_bar_set_title(header_bar, "UrbanRide Driver");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "urban_ride_driver_new");
+    gtk_window_set_title(window, "UrbanRide Driver");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
